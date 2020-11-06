9785591fabb93b57821bb0b744f1e92fdd2dcbf6
Fabricio <fabricioviazzi@gmail.com>
2020-11-06 00:54:03 -0300

    Add expo setup

---
+++ App.js
@@ -0,0 +1,24 @@
+// External Modules
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// Internal Modules
+import Example from './src/screens/Example/Example';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
