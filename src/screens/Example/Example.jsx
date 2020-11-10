348a6f1de002dd3c695c0f5af3bb58dc2c2dbc52
Fabricio <fabricioviazzi@gmail.com>
2020-11-10 12:47:15 -0300

    Add example

--- src/screens/Example/Example.jsx
+++ src/screens/Example/Example.jsx
@@ -1,10 +1,14 @@
+// External Modules
 import React from 'react';
 import { Text, View } from 'react-native';
+
+// Internal Modules
+import styles from "./Example.scss";
 
 export default function Example(props) {
   return (
     <View>
-      <Text>Sample Text</Text>
+      <Text className={styles.txt}>Example Text</Text>
     </View>
   );
 }
